ba0517e9fa80c85ad0a342ad0da3351d972726ef
Stefan <36883743+s-muenzel@users.noreply.github.com>
2018-08-10 15:03:03 +0200

    Erste nutzbare Version
    Aktuell nur Theorie, nicht getestet.

--- Zisternen-Fuellstand/Zisternen-Fuellstand.ino
+++ Zisternen-Fuellstand/Zisternen-Fuellstand.ino
@@ -10,6 +10,7 @@
 // NewPing: HC-SR04 Sensor Bibliothek
 #include <NewPing.h>
 // Anzeige (16x2 LCD per I2C)
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 ////////////////////////////////////////////
@@ -33,14 +34,24 @@
 #define INITIAL_MAX_WASSERABSTAND 100 // [cm], nur zum initialisieren (wird real etwa bei 10cm sein)
 #define INITIAL_MIN_WASSERABSTAND 101 // [cm], nur zum initialisieren (wird real etwa bei 180cm sein)
 
-#define W_VERB_UPT 1000               // Wasserverbrauch nur neu im EEPROM abspeichern, wenn mindestens 1 Kubikmeter Wasser verbraucht wurde
+#define WASSER_VERBRAUCH_SCHWELLE 3   // [cm] Wasserverbrauch nur hochzählen, wenn der Wasserspiegel mindestens um x cm gesunken ist
+
+#define WASSER_VERBRAUCH_UPDATE 1000  // [l] Wasserverbrauch nur neu im EEPROM abspeichern, wenn mindestens 1 Kubikmeter Wasser verbraucht wurde
 
 #define MAX_ABSTAND 200               // Maximaler Abstand Sensor - Wasser[cm]. Reduziert die Zeit bei einer Fehlmessung
+
+#ifdef DEBUG
+#define ZYKLUS_DAUER 1000             // Etwa alle Sekunde messen
+#else
+#define ZYKLUS_DAUER 60000            // Etwa alle Minute messen
+#endif
 
 ////////////////////////////////////////////
 // Die die globalen Variablen und Objekte
 
 NewPing Sensor(4, 5, MAX_ABSTAND);    // TRIGGER, ECHO, MAX_ABSTAND
+
+LiquidCrystal_I2C Lcd(0x27,16,2);     // I2C Adresse 0x27, 16 Zeichen, 2 Zeilen
 
 
 // Werte
@@ -55,6 +66,7 @@ unsigned long Letzter_Wasserverbrauch;// [l] - Wann wurde das EEPROM das letzte 
 
 ///////////////////////////////////////////
 // Hier beginnen die Hilfsfunktionen
+
 unsigned int Liter(int Neu, int Alt) {
 // Berechnet das vorhandene Wasser in Litern
 // Benutzung entweder (Messung neu, Messung alt):                   Wasserverbrauch    (>0 wenn Neu > Alt)
@@ -65,7 +77,7 @@ unsigned int Liter(int Neu, int Alt) {
   return (Neu - Alt) * PI *10;
 }
 
-float Fuellstand() {
+byte Fuellstand() {
 // Vorhandenes Wasser in % der Kapazität
   return (Max_Wasserabstand - Aktueller_Wasserabstand)/(Max_Wasserabstand - Min_Wasserabstand);
 }
@@ -74,14 +86,14 @@ void Wasserverbrauch_Aktualisieren(unsigned int Wasserabstand) {
 // Wasserverbrauch erhöhen, wenn seit der letzten Erhöhung der Wasserspiegel um mindestens 3cm gesunken ist (also 30*PI ~= 94 Liter weg sind)
 // Dann Letzter_Wasserabstand auf Stand bringen. Letzter_Wasserabstand auch auf Stand bringen, wenn Wasserspiegel genauso viel gestiegen ist
 // (Vorgehen soll nur die Mess-Schwankungen ausfiltern)
-  if (Aktueller_Wasserabstand-Letzter_Wasserabstand >= 3) {
+  if (Aktueller_Wasserabstand-Letzter_Wasserabstand >= WASSER_VERBRAUCH_SCHWELLE) {
     Wasserverbrauch += Liter(Aktueller_Wasserabstand, Letzter_Wasserabstand);
     Letzter_Wasserabstand = Aktueller_Wasserabstand;
-  } else if (Aktueller_Wasserabstand-Letzter_Wasserabstand <= -3) {
+  } else if (Aktueller_Wasserabstand-Letzter_Wasserabstand <= -WASSER_VERBRAUCH_SCHWELLE) {
     Letzter_Wasserabstand = Aktueller_Wasserabstand;    
   }
-// Wenn mindestens W_VERB_UPT Liter verbraucht wurden, EEPROM neu schreiben (Filter, um Schreibzyklen zu minimieren)
-  if (Wasserverbrauch - Letzter_Wasserverbrauch > W_VERB_UPT) {
+// Wenn mindestens WASSER_VERBRAUCH_UPDATE Liter verbraucht wurden, EEPROM neu schreiben (Filter, um Schreibzyklen zu minimieren)
+  if (Wasserverbrauch - Letzter_Wasserverbrauch > WASSER_VERBRAUCH_UPDATE) {
     EEPROM.put(EEPROM_VERW, Wasserverbrauch);
     Letzter_Wasserverbrauch = Wasserverbrauch;
   }
@@ -107,6 +119,7 @@ void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
 #endif
 
+// Initialsieren der Werte, aus EEPROM und Sensor
   Lese_EEPROM();
   // Wirklich Initial: Falls noch nie Max/Min-Werte gesetzt wurden, einmalig setzen
   if(Max_Wasserabstand == 0) {
@@ -116,6 +129,10 @@ void setup() {
   unsigned int mSekunden = Sensor.ping_median(5);
   Letzter_Wasserabstand = Sensor.convert_cm(mSekunden);
   Letzter_Wasserverbrauch = Wasserverbrauch;
+
+// Start des Displays
+  Lcd.init();
+  Lcd.backlight();
 }
 
 void loop() {
@@ -135,10 +152,31 @@ void loop() {
   Wasserverbrauch_Aktualisieren(Aktueller_Wasserabstand);
   
   // Darstellen
-  PRINT("Entfernung Wasserspiegel: ");
-  PRINT(Sensor.ping_cm());
-  PRINTLN("cm ");
-  delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Lcd.clear();
+  Lcd.setCursor(0,0);
+  Lcd.print("Inhalt:");
+  Lcd.print(Liter(Max_Wasserabstand, Aktueller_Wasserabstand));
+  Lcd.print("l");
+  Lcd.print(Fuellstand());
+  Lcd.print("%");
+  Lcd.setCursor(0,1);
+  Lcd.print("Verbrauch:");
+  Lcd.print(Wasserverbrauch);
+  
+  
+  // Wenn DEBUG, dann die Werte auf Seriell ausgeben
+  PRINT("Abst. Wasser: "); PRINT(Min_Wasserabstand);
+  PRINT("cm<"); PRINT(Aktueller_Wasserabstand);
+  PRINT("cm("); PRINT(Letzter_Wasserabstand);
+  PRINT("cm)<"); PRINT(Max_Wasserabstand);
+  PRINTLN("cm");
+
+  PRINT("Verb. Wasser: "); PRINT(Wasserverbrauch);
+  PRINT("l (EEPROM: "); PRINT(Letzter_Wasserverbrauch);
+  PRINTLN("l)");
+
+  
+  delay(ZYKLUS_DAUER);
 }
 
 
